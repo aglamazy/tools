5dc16a32c5df36e126aebfa38aaff731ec1e7fef
Yaakov Aglamaz <yaakov.aglamaz@gmail.com>
2025-11-19 14:58:06 +0200

    chore: add rewrites and favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/he', destination: '/' },
+      { source: '/he/:path*', destination: '/:path*' },
+    ]
+  },
+}
 
 export default nextConfig
