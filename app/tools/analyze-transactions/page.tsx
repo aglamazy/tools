c67a66f4ecc828ff7c337b5c90e179996644d2a9
Yaakov Aglamaz <yaakov.aglamaz@gmail.com>
2025-12-07 23:19:11 +0200

    remove unused files

--- app/tools/analyze-transactions/page.tsx
+++
@@ -1,9 +0,0 @@
-import AnalyzeTransactions from '@/app/components/AnalyzeTransactions'
-
-export default function AnalyzeTransactionsPage() {
-  return (
-    <div className="tool-page" dir="rtl">
-      <AnalyzeTransactions />
-    </div>
-  )
-}
